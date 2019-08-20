8efef8b130baa7c220694280cae30faaf40595e8
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2019-08-20 20:20:37 +0100

    Signup and login working

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Item, Form, Input, Button, Label } from "native-base";
+import * as firebase from "firebase";
 
-export default function App(){
+var config = {
+    apiKey: "AIzaSyC11mTUJ0TdZIDctB1VwQ-m2Rt_oEuvvvA",
+    authDomain: "rosy-griffin-228411.firebaseapp.com",
+    databaseURL: "https://rosy-griffin-228411.firebaseio.com",
+    projectId: "rosy-griffin-228411",
+    storageBucket: "rosy-griffin-228411.appspot.com",
+     messagingSenderId: "147862235797"
+};
+
+firebase.initializeApp(config);
+
+export default class App extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  SignUp = (email, password) => {
+    try {
+      firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => { 
+                 console.log(user);
+           });
+    } catch (error) {
+          console.log(error.toString(error));
+        }
+      };
+
+  Login(email, password){
+    try {
+      firebase
+         .auth()
+         .signInWithEmailAndPassword(email, password)
+         .then(res => {
+             console.log(res.user.email);
+      });
+    } catch (error) {
+          console.log(error.toString(error));
+        }
+      };
+
+
+
+render() {
     return (
       <Container>
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
-            <Input autoCapitalize="none" autoCorrect={false} />
+            <Input autoCapitalize="none" 
+            autoCorrect={false} 
+            onChangeText={email => this.setState({ email })}
+            />
           </Item>
           <Item floatingLabel>
             <Label>Password</Label>
@@ -16,14 +68,22 @@ export default function App(){
               secureTextEntry={true}
               autoCapitalize="none"
               autoCorrect={false}
+              onChangeText={password => this.setState({ password })}
             />
           </Item>
-          <Button full rounded success>
+          <Button full rounded success
+          onPress={() => this.Login(this.state.email, this.state.password)}>
             <Text>Login</Text>
+          </Button>
+
+          <Button full rounded default style={{ marginTop: 20 }}
+          onPress={() => this.SignUp(this.state.email, this.state.password)}> 
+          <Text>Signup</Text>
           </Button>
         </Form>
       </Container>
     );
+}
 }
 
 const styles = StyleSheet.create({
@@ -34,3 +94,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
